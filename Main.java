1ea617e97f865a75d5bc2f3520fb50a4b5a8c2b4
CreativeSeba <112626875+CreativeSeba@users.noreply.github.com>
2024-03-22 08:58:22 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package pack;
+
+public class Main{
+    public static void main(String[] args) {
+        RBTree tree = new RBTree();
+        tree.add(4, 6);
+        tree.add(17, 6);
+        tree.add(1, 6);
+        tree.add(24, 8);
+        tree.add(15, 6);
+        tree.add(2, 6);
+    }
+}
